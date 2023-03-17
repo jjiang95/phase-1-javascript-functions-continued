e508765a866326b62071476bc5e50028af16fe70
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-03-17 10:25:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(doWhat = "roller-skate") {
+    return `This Saturday, I want to ${doWhat}!`;
+}
+
+function mondayWork(doWhat = "go to the office") {
+    return `This Monday, I will ${doWhat}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function (input="special") {
+        return `You are ${flair}${input}${flair}!`
+    }
+
+}
